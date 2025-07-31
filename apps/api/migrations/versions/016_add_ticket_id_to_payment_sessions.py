1971395855db897b75cab24d25c1e12fa06df91d
alexquintans <alexgeo1403@gmail.com>
2025-07-31 19:39:50 -0300

    Corrigir dependência da migration 016 para apontar para 015

--- apps/api/migrations/versions/016_add_ticket_id_to_payment_sessions.py
+++ apps/api/migrations/versions/016_add_ticket_id_to_payment_sessions.py
@@ -11,7 +11,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = '016'
-down_revision = None
+down_revision = '015'
 branch_labels = None
 depends_on = None
 
